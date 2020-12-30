af509fd41282ea739cd1572e0039a0c858cc4704
Dang310397 <75921332+Dang310397@users.noreply.github.com>
2020-12-30 20:18:31 +0700

    Update urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('selectcurrency', views.selectcurrency, name='selectcurrency'),
     path('savelangcur', views.savelangcur, name='savelangcur'),
 
-    # path('blog_detail/<int:id>/', BlogViews.blog_detail, name='blog_detail'),
+    path('blog_detail/<int:id>/', BlogViews.blog_detail, name='blog_detail'),
     path('about/',views.about, name='about'),
     path('faq/',views.faq, name='faq'),
     path('contact/',views.contact, name='contact'),
